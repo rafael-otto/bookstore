57ec9a3191fdbfe516c4c946f99e7252f5f4d217
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-04-24 09:23:42 -0300

    FEAT: Imagens dos livros adicionadas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,56 +5,56 @@ const produtos = [
         titulo: 'Chain of Iron: Volume 2',
         autor: 'Cassandra Clare',
         preco: 23.24,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/81IP261kwlL._AC_UF1000,1000_QL80_.jpg',
     },
     {
         id: 2,
         titulo: 'Chain of Thorns',
         autor: 'Cassandra Clare',
         preco: 23.24,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/91298Zw5GdL._AC_UF1000,1000_QL80_.jpg',
     },
     {
         id: 3,
         titulo: 'City of Fallen Angels',
         autor: 'Cassandra Clare',
         preco: 13.94,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/815MzJpG6iL._AC_UF1000,1000_QL80_.jpg',
     },
     {
         id: 4,
         titulo: 'Nona the Ninth',
         autor: 'Cassandra Clare',
         preco: 16.84,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/81a2wDWhyNL._AC_UF1000,1000_QL80_.jpg',
     },
     {
         id: 5,
         titulo: 'Harlem Shuffle',
         autor: 'Colson Whitehead',
         preco: 26.92,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://scottsemegran.com/images/stories/book_reviews/harlem_shuffle_by_colson_whitehead.jpg',
     },
     {
         id: 6,
         titulo: 'Two Old Women',
         autor: 'Velma Wallis',
         preco: 13.95,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/81Yo0MecUrL.jpg',
     },
     {
         id: 7,
         titulo: 'Carrie Soto Is Back',
         autor: 'Taylor Jenkins Reid',
         preco: 26.04,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/916WmQau7IL.jpg',
     },
     {
         id: 8,
         titulo: 'Book Lovers',
         autor: 'Emily Henry',
         preco: 15.81,
-        capa: 'https://placehold.co/600x400.png',
+        capa: 'https://m.media-amazon.com/images/I/71Xy4AL7jKL.jpg',
     },
 ];
 </script>
@@ -74,7 +74,7 @@ const produtos = [
       </div>
 
       <div class="image">
-        <img src="" alt="Livro">
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1724071848i/32572089.jpg" alt="Livro">
       </div>
     </section>
     <section class="barraCentral">
@@ -96,7 +96,7 @@ const produtos = [
     <section class="books">
       <h2>Lançamentos</h2>
       <div v-for="book in produtos" :key="book.id" class="book">
-        <img src="" alt="">
+        <img :src="book.capa" alt="">
         <p class="title">{{ book.titulo }}</p>
         <p class="autor">{{ book.autor }}</p>
         <p class="price">R$ {{ book.preco }}</p>
