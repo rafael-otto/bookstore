434cf5729fbf68a1f6779fee2ead15b6f1463303
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2025-04-24 08:43:36 -0300

    FEAT: components's archives header and footer; header's template finished

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div>
+        <h1>IF Books</h1>
+        <h3>Apreço a leitura</h3>
+        </div>
+        <div>
+            <ul>
+                <li>Termos</li>
+                <li>Equipe</li>
+                <li>Envio</li>
+                <li>Devoluções</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <a href=""><img src="../../public/header img/Icon.png" alt=""></a>
+                </li>
+                <li>
+                    <a href=""><img src="../../public/header img/Icon2.png" alt=""></a>
+                </li>
+                <li>
+                    <a href=""><img src="../../public/header img/Icon3.png" alt=""></a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+<style scoped>
+    header {
+        font-size: 2vw;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
+import HeaderView from '../components/HeaderView.vue';
 </script>
 
 <template>
   <main>
+    <HeaderView />
     <h1>This is home page</h1>
     <TheWelcome />
   </main>
