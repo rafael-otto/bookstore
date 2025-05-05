2a9aa404b2e164a2f42413e9995c5e8d26a00c28
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2025-05-05 12:03:28 -0300

    FEAT: footer's css finished and header's css iniciated

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -56,6 +56,7 @@ footer {
     background-color: #27AE60;
     color: white;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
+    display: none;
 }
 .geral {
     display: flex;
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,15 @@
 <template>
     <header>
-        <div>
-        <h1>IF Books</h1>
-        <h3>Apreço a leitura</h3>
+        <div class="logotipo">
+            <h1>IF Books</h1>
+            <hr>
+            <h3>Apreço a<br>leitura</h3>
         </div>
-        <div>
+        <div class="barrapesquisa">
+            <input type="text" name="pesquisar" id="pesquisar" placeholder="Pesquisar">
+            <button><img src="../../public/header-img/Search.png" alt="botão de pesquisa"></button>
+        </div>
+        <div class="opcoes">
             <ul>
                 <li>Termos</li>
                 <li>Equipe</li>
@@ -12,21 +17,67 @@
                 <li>Devoluções</li>
             </ul>
         </div>
-        <div>
+        <div class="direcionamentos">
             <ul>
                 <li>
-                    <a href=""><img src="../../public/header-img/Icon.png" alt=""></a>
+                    <a href=""><img src="../../public/header-img/Icon.png"
+                            alt="icone de direcionamento para o carrinho de compras"></a>
                 </li>
                 <li>
-                    <a href=""><img src="../../public/header-img/Icon2.png" alt=""></a>
+                    <a href=""><img src="../../public/header-img/Icon2.png"
+                            alt="icone de direcionamento para os produtos favoritos"></a>
                 </li>
                 <li>
-                    <a href=""><img src="../../public/header-img/Icon3.png" alt=""></a>
+                    <a href=""><img src="../../public/header-img/Icon3.png"
+                            alt="icone de direcionamento para a página do usuário"></a>
                 </li>
             </ul>
         </div>
     </header>
 </template>
 <style scoped>
-    
+header {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: 1vw 5vw 1vw 5vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+/*Logotipo*/
+.logotipo {
+    display: flex;
+    align-items: center;
+}
+.logotipo h1 {
+    padding-right: 0.5vw;
+}
+.logotipo hr {
+    border: 1px solid #27AE60;
+    height: 32px;
+}
+.logotipo h3 {
+    padding-left: 0.5vw;
+    color: rgba(39, 174, 96, 0.6);
+}
+/*Pesquisar*/
+.barrapesquisa {
+    padding-left: 5vw;
+}
+.barrapesquisa input,
+.barrapesquisa button {
+    border: none;
+}
+.barrapesquisa input {
+    background-color: #f1f1f1;
+    padding: 0.4vw 5vw 0.4vw 0.4vw;
+    font-size: 1.1rem;
+
+}
+.barrapesquisa ::placeholder {
+    color: #b8b8b8;
+}
+.barrapesquisa button {
+    background-color: #00000000;
+    transform: translate(-2vw, 0.35vw);
+    }
 </style>
