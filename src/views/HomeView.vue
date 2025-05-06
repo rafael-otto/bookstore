511cdcbebb66a079fa38318517aeac1b5cc15e94
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-05-06 09:31:16 -0300

    STYLE: Fazendo o css da home, banner, barra central e lançamentos #14

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -62,12 +62,12 @@ const produtos = [
 <template>
   <main>
     <section class="banner">
-      <div class="autorMes">
-        <p>Autor de Abril</p>
-      </div>
       <div class="text">
+        <div class="autorMes">
+          <p>Autor de Abril</p>
+        </div>
         <h1>Eric-Emanuel Schmitt</h1>
-        <p>
+        <p class="descricao">
           Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and
           in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been
           translated into over 40 languages.
@@ -78,36 +78,143 @@ const produtos = [
       </div>
 
       <div class="image">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1724071848i/32572089.jpg" alt="Livro">
+        <img
+          src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNM-WgqLF_HtYa8xUec1vEU-q4KQbHi9tgac-1WG5-cir6NZ-qG6NquQE3WBdwDZyEsF7Jp4uRd_KwZhVnyUn_3AGWeUoFUk8UWt8LOsXLBx_b6R5aMiKpsNZlVoiWciWyx1CyeGN972c/s640/Schmitt_Nocognia_3D.jpg"
+          alt="Livro">
+        <p>*within the stock limit</p>
       </div>
     </section>
     <section class="barraCentral">
       <ul>
         <li>
-          <span><font-awesome-icon icon="fa-solid fa-truck" /></span>
+          <span><img src="../../public/home-img/Truck.png" alt="truck"></span>
           <p>Frete grátis para SC</p>
         </li>
-        <li>
-          <span><font-awesome-icon icon="fa-solid fa-star" /></span>
+        <li class="meio">
+          <span><img src="../../public/home-img/Star.png" alt="star"></span>
           <p>Livros recomendados</p>
         </li>
         <li>
-          <span><font-awesome-icon icon="fa-solid fa-book-open" /></span>
+          <span><img src="../../public/home-img/Book-open.png" alt="Book Open"></span>
           <p>Mais vendidos</p>
         </li>
       </ul>
+
     </section>
     <section class="books">
-      <h2>Lançamentos</h2>
-      <div v-for="book in produtos" :key="book.id" class="book">
-        <img :src="book.capa" alt="">
-        <p class="title">{{ book.titulo }}</p>
-        <p class="autor">{{ book.autor }}</p>
-        <p class="price">R$ {{ book.preco }}</p>
-        <button>
-          <span><font-awesome-icon icon="fa-solid fa-cart-shopping" /></span>Comprar
-        </button>
+      <div>
+        <h2>Lançamentos</h2>
+      </div>
+      <div class="tabela">
+        <div v-for="book in produtos" :key="book.id" class="book">
+          <img :src="book.capa" alt="">
+          <p class="title">{{ book.titulo }}</p>
+          <p class="autor">{{ book.autor }}</p>
+          <p class="price">R$ {{ book.preco }}</p>
+          <button>
+            <span><font-awesome-icon icon="fa-solid fa-cart-shopping" /></span>Comprar
+          </button>
+        </div>
       </div>
     </section>
   </main>
 </template>
+<style scoped>
+main {
+
+  .banner {
+    margin: 1vw 6vw 1vw 6vw;
+    display: flex;
+
+    .text {
+      h1 {
+        font-size: 3.5rem;
+        font-weight: 900;
+        margin-top: 2vw;
+        margin-bottom: 2vw;
+      }
+
+      p.descricao {
+        margin-right: 15vw;
+      }
+
+      .autorMes {
+        margin-top: 3vw;
+        padding: 0.5vw 0.3vw 0.5vw 0.3vw;
+        border: 1px solid #27AE60;
+        width: 10vw;
+        text-align: center;
+
+        p {
+          color: #27AE60;
+          font-size: 1.1rem;
+        }
+      }
+    }
+
+    .image {
+      p {
+        text-align: right;
+      }
+
+    }
+  }
+
+  .barraCentral {
+
+    padding: 3vw 0 3vw 0;
+    border-bottom: 1px solid #27AE60;
+    border-top: 1px solid #27AE60;
+    width: 100vw;
+
+    ul {
+      list-style: none;
+      display: flex;
+      justify-content: space-around;
+
+      li {
+        display: flex;
+        align-items: center;
+
+        p {
+          font-size: 1.5rem;
+          font-weight: 600;
+          margin-left: 1vw;
+        }
+      }
+
+      li.meio {
+        border-right: 1px solid #27AE60;
+        border-left: 1px solid #27AE60;
+        padding: 0 8vw 0 8vw
+      }
+    }
+  }
+
+  .books {
+    margin: 1vw 6vw 1vw 6vw;
+
+    h2 {
+      font-size: 2.5rem;
+      font-weight: 600;
+      color: #27AE60;
+      margin: 3vw 0 3vw 0;
+    }
+
+    .tabela {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      .book {
+        display: grid;
+
+        img {
+          height: 30vw;
+          width: 20vw;
+        }
+      }
+    }
+  }
+}
+</style>
